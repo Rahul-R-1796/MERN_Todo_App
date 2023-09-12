a5e08453b88d9a061b4991cdbdc4b203cb5c00a3
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-12 11:48:50 +0530

    Update App.js

--- ToDo_FrontEnd/src/App.js
+++ ToDo_FrontEnd/src/App.js
@@ -1,47 +1,70 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "./components/Preloader";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import ScrollToTop from "./components/ScrollToTop";
+import axios from "axios";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
+    axios.get("/todos").then((res) => setTodos(res.data));
   }, []);
 
+  const addTodo = () => {
+    axios.post("/todos", { text: newTodo, completed: false }).then((res) => {
+      setTodos([...todos, res.data]);
+      setNewTodo("");
+    });
+  };
+
+  const updateTodo = (id, text, completed) => {
+    axios.put(`/todos/${id}`, { text, completed }).then((res) => {
+      const updatedTodos = todos.map((todo) =>
+        todo._id === id ? res.data : todo
+      );
+      setTodos(updatedTodos);
+    });
+  };
+
+  const deleteTodo = (id) => {
+    axios.delete(`/todos/${id}`).then(() => {
+      const updatedTodos = todos.filter((todo) => todo._id !== id);
+      setTodos(updatedTodos);
+    });
+  };
+
   return (
-    <Router>
-      {load ? (
-        <Preloader load={load} />
-      ) : (
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
-          <Navbar />
-          <ScrollToTop />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/project" component={Projects} />
-            <Route path="/about" component={About} />
-            <Route path="/resume" component={Resume} />
-          </Switch>
-          <Footer />
-        </div>
-      )}
-    </Router>
+    <div className="App">
+      <h1>To-Do App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={(e) =>
+                updateTodo(todo._id, todo.text, e.target.checked)
+              }
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
