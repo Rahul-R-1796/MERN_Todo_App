5af543e1129c330d4ba164dae44d9f97ee13fb0d
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-12 13:12:19 +0530

    Update server.js

--- Todo_BackEnd/server.js
+++ Todo_BackEnd/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/todoapp", {
+mongoose.connect("mongodb+srv://rahulrajenderan96:rahulrajenderan96@cluster0.xrqprey.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
 const todoSchema = new mongoose.Schema({
@@ -21,6 +22,42 @@ const todoSchema = new mongoose.Schema({
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
+
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+  
+    // Replace this with your user data retrieval logic (e.g., from a database)
+    const user = { id: 1, username: "user", password: "1234" }; // Password: demo
+  
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+  
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ id: user.id }, "your-secret-key", {
+        expiresIn: "1h", // expiration time 
+      });
+      res.json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid password" });
+    }
+  });
+  
+  // Protect routes with authentication middleware
+  const authenticate = (req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) {
+      return res.status(401).json({ error: "Access denied" });
+    }
+  
+    jwt.verify(token, "your-secret-key", (err, user) => {
+      if (err) {
+        return res.status(403).json({ error: "Invalid token" });
+      }
+      req.user = user;
+      next();
+    });
+  };
 
 app.get("/todos", async (req, res) => {
   const todos = await Todo.find();
