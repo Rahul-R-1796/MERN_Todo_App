e76e925c20f3f651c6ac12d76bd6aa6f6725759b
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-12 12:05:35 +0530

    Update App.js

--- ToDo_FrontEnd/src/App.js
+++ ToDo_FrontEnd/src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Login from "./components/Login";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      axios.get("/todos").then((res) => setTodos(res.data));
+    }
+  }, [isLoggedIn]);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
+  const addTodo = () => {
+    axios.post("/todos", { text: newTodo, completed: false }).then((res) => {
+      setTodos([...todos, res.data]);
+      setNewTodo("");
+    });
+  };
+
+  const updateTodo = (id, text, completed) => {
+    axios.put(`/todos/${id}`, { text, completed }).then((res) => {
+      const updatedTodos = todos.map((todo) =>
+        todo._id === id ? res.data : todo
+      );
+      setTodos(updatedTodos);
+    });
+  };
+
+  const deleteTodo = (id) => {
+    axios.delete(`/todos/${id}`).then(() => {
+      const updatedTodos = todos.filter((todo) => todo._id !== id);
+      setTodos(updatedTodos);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={(e) =>
+                updateTodo(todo._id, todo.text, e.target.checked)
+              }
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
